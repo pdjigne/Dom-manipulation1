5f4b48f19082f912c6949504bc6b229eb2e2d898
pdjigne <132499480+pdjigne@users.noreply.github.com>
2023-12-13 14:18:22 -0500

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,17 @@
+// Menu data structure
+var menuLinks = [
+  { text: 'about', href: '/about' },
+  { text: 'catalog', href: '/catalog' },
+  { text: 'orders', href: '/orders' },
+  { text: 'account', href: '/account' },
+];
+
+import "./styles.css";
+const mainEl = document.getElementById("mainEl");
+mainEl.classList.add("flex-ctr");
+const topMenuEl = document.querySelector("#top-menu");
+topMenuEl.classList.add("flex-around");
+
+
+
+
